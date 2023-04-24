a3a26f39dbed699c375d04903c87594b5ca940a2
zhibin <2608606700@qq.com>
2023-04-25 01:39:45 +0800

    v1

---
+++ docs/zh/mongoDBDoc/mongoDB.md
@@ -0,0 +1,7 @@
+# 1. 安装
+
+> [MongoDB的安装配置教程（很详细，你想要的都在这里）_mongodb安装_狮子座的男孩的博客-CSDN博客](https://blog.csdn.net/weixin_43405300/article/details/120017878)
+
+6.0 后需要 安装 shell 
+
+[MongoDB 6.x 在 Windows 和 Linux 下的安装教程（详细） - 掘金](https://juejin.cn/post/7163684333616644103)
--- modern.config.ts
+++ modern.config.ts
@@ -1,62 +1,81 @@
-import { defineConfig } from "@modern-js/doc-tools";
-import path from "path";
+import { defineConfig } from '@modern-js/doc-tools';
+import path from 'path';
 
-function getI18nHelper(lang: "zh" | "en") {
-  const cn = lang === "zh";
+function getI18nHelper(lang: 'zh' | 'en') {
+  const cn = lang === 'zh';
   // 默认语言为中文，如果是英文，需要加上 /en 前缀
   // The default language is Chinese, if it is English, you need to add the /en prefix
-  const prefix = cn ? "" : "/en";
+  const prefix = cn ? '' : '/en';
   const getLink = (str: string) => `${prefix}${str}`;
   const getText = (cnText: string, enText: string) => (cn ? cnText : enText);
   return { getText, getLink };
 }
-
-function getNavConfig(lang: "zh" | "en") {
+// index
+function getNavConfig(lang: 'zh' | 'en') {
   const { getText, getLink } = getI18nHelper(lang);
   return [
     {
-      text: getText("首页", "Home"),
-      link: getLink("/"),
+      text: getText('首页', 'Home'),
+      link: getLink('/'),
     },
     {
-      text: getText("多级菜单", "Multi-level"),
+      text: getText('code', 'code'),
       items: [
         {
-          text: getText("子菜单1", "Submenu1"),
-          link: getLink("/"),
+          text: getText('Java', 'Java'),
+          link: getLink('/javaDoc/Java'),
         },
         {
-          text: getText("子菜单2", "Submenu2"),
-          link: getLink("/"),
+          text: getText('React', 'React'),
+          link: getLink('/reactDoc/react'),
+        },
+      ],
+    },
+    {
+      text: getText('项目实践', 'ProjectPractice'),
+      items: [
+        {
+          text: getText('全栈之巅', 'topfullstack'),
+          link: getLink('/topfullstackDoc/topfullstack'),
+        },
+      ],
+    },
+    {
+      text: getText('调试问题', 'DebugProblem'),
+      items: [
+        {
+          text: getText('MongoDB', 'MongoDB'),
+          link: getLink('/mongoDBDoc/mongoDB'),
         },
       ],
     },
   ];
 }
 
-function getSidebarConfig(lang: "zh" | "en") {
+// context
+function getSidebarConfig(lang: 'zh' | 'en') {
   const { getText, getLink } = getI18nHelper(lang);
   // 注: 侧边栏配置可以嵌套，子菜单字段为 items
   // Note: The sidebar configuration can be nested, and the sub-menu field is items
   return {
-    "/": [
+    '/': [
       {
-        text: getText("快速上手", "Quick Start"),
-        link: getLink("/guide/getting-started"),
+        text: getText('快速上手', 'Quick Start'),
+        link: getLink('/guide/getting-started'),
         items: [
           {
-            text: getText("安装", "Install"),
-            link: getLink("/guide/install"),
+            text: getText('安装', 'Install'),
+            link: getLink('/guide/install'),
           },
         ],
       },
       {
-        text: getText("基础", "Basic"),
-        link: getLink("/guide/basic"),
+        text: getText('基础', 'Basic'),
+        link: getLink('/guide/basic'),
       },
       {
-        text: getText("进阶", "Advanced"),
-        link: getLink("/guide/advanced"),
+        text: getText('进阶', 'Advanced'),
+        link: getLink('/guide/advanced'),
       },
     ],
   };
@@ -64,43 +83,51 @@ function getSidebarConfig(lang: "zh" | "en") {
 
 export default defineConfig({
   doc: {
-    root: path.join(__dirname, "docs"),
-    title: "A awesome project",
-    description: "A awesome project description",
-    logo: "https://lf3-static.bytednsdoc.com/obj/eden-cn/zq-uylkvT/ljhwZthlaukjlkulzlp/logo-2x-text-0104.png",
-    icon: "https://lf3-static.bytednsdoc.com/obj/eden-cn/zq-uylkvT/ljhwZthlaukjlkulzlp/logo-1x-0104.png",
+    root: path.join(__dirname, 'docs'),
+    title: '在争渡！再为热爱续命...',
+    description: '在争渡！再为热爱续命...',
+    logo: 'https://lf3-static.bytednsdoc.com/obj/eden-cn/zq-uylkvT/ljhwZthlaukjlkulzlp/logo-2x-text-0104.png',
+    icon: 'https://lf3-static.bytednsdoc.com/obj/eden-cn/zq-uylkvT/ljhwZthlaukjlkulzlp/logo-1x-0104.png',
     // 默认语言
     // Default language
-    lang: "zh",
+    lang: 'zh',
     themeConfig: {
       footer: {
         // 页脚的文案
         // Footer text
-        message: "© 2023 Bytedance Inc. All Rights Reserved.",
+        message: '暂定...',
       },
       // 不同语言的配置
       // Configuration for different languages
       locales: [
         {
-          lang: "zh",
-          title: "一个很棒的项目",
-          description: "一个很棒的项目描述",
-          nav: getNavConfig("zh"),
-          sidebar: getSidebarConfig("zh"),
+          lang: 'zh',
+          title: '一个很棒的项目',
+          description: '一个很棒的项目描述',
+          nav: getNavConfig('zh'),
+          sidebar: getSidebarConfig('zh'),
           // 语言切换按钮的文案
           // Language switch button text
-          label: "简体中文",
+          label: '简体中文',
         },
         {
-          lang: "en",
-          title: "A awesome project",
-          description: "A awesome project description",
-          nav: getNavConfig("en"),
-          sidebar: getSidebarConfig("en"),
-          label: "English",
+          lang: 'en',
+          title: 'A awesome project',
+          description: 'A awesome project description',
+          nav: getNavConfig('en'),
+          sidebar: getSidebarConfig('en'),
+          label: 'English',
+        },
+      ],
+      socialLinks: [
+        // 链接
+        {
+          icon: 'github',
+          mode: 'link',
+          content: 'https://github.com/liu4509',
         },
       ],
     },
   },
-  plugins: ["@modern-js/doc-tools"],
+  plugins: ['@modern-js/doc-tools'],
 });
